ffd91a123a4a9a38a5f9114d2c8702ea0d58f8f7
ianj090 <ianjenatz@ufm.edu>
2020-09-12 16:29:09 -0600

    made flask run home and added requests

--- MindFlash.py
+++ MindFlash.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+import requests, sys, json
+
+# news_request = "https://www.example.com"
+
+# try:
+#     news_content = requests.get(news_request).text
+# except:
+#     print(f"unable to get {news_request}")
+#     sys.exit(1)
 
 app = Flask(__name__)
 
-@app.route("/") # main page
-def info():
+# news_cards = json.loads(news_content)
+
+# MYSQL Handlers
+# db = MYSQL database
+
+@app.route("/", methods=["GET", "POST", "DELETE"]) # main page
+def home():
     return render_template(
-    "main.html")
+    "home.html",
+    # news_cards = news
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
