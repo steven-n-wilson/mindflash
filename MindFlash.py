2f119e13108ceb802c50acc79c6df120be871484
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-13 22:09:17 -0600

    fixed flask css issues

--- MindFlash.py
+++ MindFlash.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-import requests, sys, json
+import requests
+import sys
+import json
 
 # news_request = "https://www.example.com"
 
@@ -16,12 +18,15 @@ app = Flask(__name__)
 # MYSQL Handlers
 # db = MYSQL database
 
-@app.route("/", methods=["GET", "POST", "DELETE"]) # main page
+
+@app.route("/", methods=["GET", "POST", "DELETE"])  # main page
 def home():
     return render_template(
-    "index.html",
-    # news_cards = news
+        "index.html",
+        # news_cards = news
     )
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug = True
+    app.run(debug=debug)
