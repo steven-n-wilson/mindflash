b1104083ab72fa6a06b521531a0888f48bbb9e2b
DavidCorzo <davidcorzo@ufm.edu>
2020-10-05 11:16:40 -0600

    2020-10-05

--- card.py
+++ card.py
@@ -1,11 +1,55 @@
+class card: 
+    def __init__(self,
+            width   :float  =None,
+            height  :float  =None,
+            color   :str    =None,
+            font    :str    =None,
+            text    :str    =None,
+            media_f :str    =None
+        ):
+        self.color      = color
+        self.width      = width
+        self.height     = height
+        self.font       = font
+        self.text       = text 
+        self.media_f    = media_f # Media file.
 
-class card_base:
-    def __init__(self, frontside, backside):
-        self.front = frontside
-        self.back = backside
-    
-    def front(self):
-        return front
+    def html(self):
+        pass
 
-    def back(self):
-        return back
+    def __str__(self):
+        string = str()
+        for k,v in self.__dict__.items(): 
+            string += str(k) + " " + str(v) + "\n"
+        return string
+
+    # Setters.
+    def set_color(self,color):
+        self.color = color
+    def set_width(self,width):
+        self.width = width
+    def set_height(self,height):
+        self.height = height
+    def set_font(self,font):
+        self.font = font
+    def set_text(self,text):
+        self.text = text
+    def set_media_f(self,media_f):
+        self.media_f = media_f
+    # Getters.
+    def get_color(self):
+        return self.color
+    def get_width(self):
+        return self.width
+    def get_height(self):
+        return self.height
+    def get_font(self):
+        return self.font
+    def get_text(self):
+        return self.text
+    def get_media_f(self):
+        return self.media_f
+
+
+
+
---
+++ card_base.py
@@ -0,0 +1,11 @@
+
+class card_base:
+    def __init__(self, frontside, backside):
+        self.front = frontside
+        self.back = backside
+    
+    def front(self):
+        return self.front
+
+    def back(self):
+        return self.back
