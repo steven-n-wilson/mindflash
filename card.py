aea843e3aa9cd28ec99d7622596aa0021f24ac44
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-10-11 13:04:50 -0600

    formatted

--- card.py
+++ card.py
@@ -1,51 +1,62 @@
-class card: 
+class card:
     def __init__(self,
-            width   :float  =None,
-            height  :float  =None,
-            color   :str    =None,
-            font    :str    =None,
-            text    :str    =None,
-            media_f :str    =None
-        ):
-        self.color      = color
-        self.width      = width
-        self.height     = height
-        self.font       = font
-        self.text       = text 
-        self.media_f    = media_f # Media file.
+                 width: float = None,
+                 height: float = None,
+                 color: str = None,
+                 font: str = None,
+                 text: str = None,
+                 media_f: str = None
+                 ):
+        self.color = color
+        self.width = width
+        self.height = height
+        self.font = font
+        self.text = text
+        self.media_f = media_f  # Media file.
 
     def html(self):
         pass
 
     def __str__(self):
         string = str()
-        for k,v in self.__dict__.items(): 
+        for k, v in self.__dict__.items():
             string += str(k) + " " + str(v) + "\n"
         return string
 
     # Setters.
-    def set_color(self,color):
+    def set_color(self, color):
         self.color = color
-    def set_width(self,width):
+
+    def set_width(self, width):
         self.width = width
-    def set_height(self,height):
+
+    def set_height(self, height):
         self.height = height
-    def set_font(self,font):
+
+    def set_font(self, font):
         self.font = font
-    def set_text(self,text):
+
+    def set_text(self, text):
         self.text = text
-    def set_media_f(self,media_f):
+
+    def set_media_f(self, media_f):
         self.media_f = media_f
     # Getters.
+
     def get_color(self):
         return self.color
+
     def get_width(self):
         return self.width
+
     def get_height(self):
         return self.height
+
     def get_font(self):
         return self.font
+
     def get_text(self):
         return self.text
+
     def get_media_f(self):
         return self.media_f
