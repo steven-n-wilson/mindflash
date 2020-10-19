b518c040c032fb60c9ecc87d97a9ab56d3b3de35
ianj090 <ianjenatz@ufm.edu>
2020-10-18 20:42:07 -0600

    Added addClass functionality and basic jinja loop

--- MindFlash.py
+++ MindFlash.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from card_base import card_base
+from types import new_class
+from flask import Flask, render_template, request
 from card import *
 
 app = Flask(__name__)
@@ -6,11 +8,27 @@ app = Flask(__name__)
 # MYSQL Handlers
 # db = MYSQL database
 
+cards = []
 
 @app.route("/", methods=["GET", "POST", "DELETE"])  # main card view page
 def cardsView():
+    # cards.append(1)
+    # cards.append(2)
+    if request.method == "POST":
+        if "AddCard" in request.form:
+            front = request.form["exampleInputEmail1"]
+            back = request.form["exampleInputPassword1"]
+            card_object = card_base(front, back)
+            cards.append(card_object)
+            for i in cards:
+                print(i)
+                print(i.frontside)
+                print(i.backside)
+        else:
+            pass
     return render_template(
         "index.html",
+        cards_array = cards
     )
 
 @app.route("/addCard", methods=["GET", "POST", "DELETE"])  # add Card page
